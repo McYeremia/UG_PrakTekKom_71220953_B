807c608cd5dc1b434c30c8a017c25353199395f1
McYeremia <117885019+McYeremia@users.noreply.github.com>
2022-12-08 12:56:42 +0700

    Add files via upload

--- UG12_B_71220953/2_B_71220953.py
+++ UG12_B_71220953/2_B_71220953.py
@@ -1,9 +1,13 @@
 print("~~~ Selamat Datang di Kalkulator Sederhana ~~~")
 choice = str(input("Masukkan operator matematika yang ingin Anda hitung: "))
 if choice == "+":
+    a = 1
     cho2=int(input("Mau penjumlahan berapa: "))
     cho3=int(input("Print berapa banyak: "))
-    for i in range (cho2-2,cho3+1):
-        print((cho2-2), "+", i, "=", (cho2-2)+cho2)
+    for i in range (cho2,0-1): 
+        print(cho2,"+", a, "=", (cho2+a))
+while yesno == "t":
+    yesno = input("Apakah anda ingin menghitung lagi? (y/t): ")       
+    print("Terima Kasih dan Sampai Jumpa Lagi!")
 else:
     print("Maaf, Operator Matematika yang anda cari belum tersedia.")
